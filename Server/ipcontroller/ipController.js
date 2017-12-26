7c9c3019226f3e1a3baf8e1977c6876c88e2658e
Nicocovi <nico0205@hotmail.com>
2017-12-26 15:50:47 +0100

    Updated server

--- Server/ipcontroller/ipController.js
+++ Server/ipcontroller/ipController.js
@@ -1,17 +1,18 @@
 var http = require('http');
 var publicIp = require('public-ip');
+var request = require("request");
 
 function passPublicIp() {
    publicIp.v4().then(ip => {
            console.log(ip);
            //Pass actual ip
             request({
-                uri: "http://www.nicolascorpanchovillasana.com/rasp/writeip?ip="+ip,
-                method: "GET",
+                uri: "http://www.nicolascorpanchovillasana.com/rasp/writeip.php?ip="+ip,
+                method: "POST",
             }, function(error, response, body) {
                 console.log(body);
             });
    });
 }
 var time = 360;
-setInterval(sendEmail, 10*1000);
+setInterval(passPublicIp, time*1000);
