1ece0c145dc5eb8d8231d68f366cf6911e88d8b9
Nicocovi <nico0205@hotmail.com>
2017-12-27 12:45:34 +0100

    Updated ipcontroller and config

--- Server/ipcontroller/ipController.js
+++ Server/ipcontroller/ipController.js
@@ -1,18 +1,21 @@
 var http = require('http');
 var publicIp = require('public-ip');
 var request = require("request");
+var Config = require('../config/config.js');
+
+var url = Config.uri.link;
 
 function passPublicIp() {
    publicIp.v4().then(ip => {
            console.log(ip);
            //Pass actual ip
             request({
-                uri: "http://www.nicolascorpanchovillasana.com/rasp/writeip.php?ip="+ip,
+                uri: url+ip,
                 method: "POST",
             }, function(error, response, body) {
                 console.log(body);
             });
    });
 }
-var time = 360;
-setInterval(passPublicIp, time*1000);
+var time = 3600;
+setInterval(passPublicIp, 600*1000);
